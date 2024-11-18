86f9655af2a167a339cfb008bc85b2f11dee6924
Tumusiime Marvin <Tumusiimemarvin2002@gmail.com>
2024-11-17 20:54:19 -0800

    Add files via upload

---
+++ uffe.sql
@@ -0,0 +1,17 @@
+
+ 
+
+
+
+
+
+
+create table Farmer_crop(
+    FarmerID INT
+    CropID INT
+    YEILDS INT
+    Season VARCHAR(45)
+    PRIMARY KEY(FarmerID,CropID)
+    FOREIGN KEY(FarmerID) REFE
+
+)
